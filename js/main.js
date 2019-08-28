1752c6eebe0472c779927646a98b83701213cb10
Arxont <hanterokun@gmail.com>
2019-08-28 17:58:14 +0300

    section 2 padding changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
+var btn = document.getElementsByClassName("myBtn");
 var span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
   modal.style.display = "block";
