389146af48b01cc3c013ed22e27bcfaefa7b1a16
Arxont <hanterokun@gmail.com>
2019-08-29 12:07:51 +0300

    scrollable modal only done

--- js/main.js
+++ js/main.js
@@ -2,11 +2,16 @@
 var modal = document.getElementById("myModal");
 var btn = document.getElementsByClassName("myBtn");
 var span = document.getElementsByClassName("close")[0];
+const body = document.body;
 btn[0].onclick = function() {
   modal.style.display = "block";
+  body.style.height = '100vh';
+  body.style.overflowY = 'hidden';
 };
 btn[1].onclick = function() {
   modal.style.display = "block";
+  body.style.height = '100vh';
+  body.style.overflowY = 'hidden';
 };
 span.onclick = function() {
   modal.style.display = "none";
