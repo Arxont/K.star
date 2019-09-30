9754499c0ced52abecf2f6b54a2f3c9cde82ece6
Arxont <hanterokun@gmail.com>
2019-09-30 14:47:22 +0300

    send request via fetch test

--- js/main.js
+++ js/main.js
@@ -22,60 +22,24 @@ function closeModal() {
   document.getElementById("modal").style.height = "0%";
 }
 
-function openIT() {
-  document.getElementById("iT").style.width = "400px";
-  document.getElementById("iT").style.borderStyle = "solid";
+const fetchForm = document.getElementById("fetchForm");
+
+fetchForm.addEventListener("submit", function(i){
+i.preventDefault();
+
+const formData = new FormData(this);
+const sParams = new URLSearchParams();
+
+
+for (const pair of formData){
+  sParams.append(pair[0], pair[1], pair[2], pair[3]);
 }
 
-function closeIT() {
-  document.getElementById("iT").style.width = "0px";
-  document.getElementById("iT").style.borderStyle = "none";
-}
+fetch("http://80.211.107.117/sales/sales-bot.php",{
+method: 'post',
+mode:"no-cors",
+body:sParams
+})
+});
 
-function openDays() {
-  document.getElementById("days").style.width = "400px";
-  document.getElementById("days").style.borderStyle = "solid";
-}
-
-function closeDays() {
-  document.getElementById("days").style.width = "0px";
-  document.getElementById("days").style.borderStyle = "none";
-}
-
-function openHard() {
-  document.getElementById("hard").style.width = "400px";
-  document.getElementById("hard").style.borderStyle = "solid";
-}
-
-function closeHard() {
-  document.getElementById("hard").style.width = "0px";
-  document.getElementById("hard").style.borderStyle = "none";
-}
-
-
-
-/*
-var modal = document.getElementById("Modal");
-var btn = document.getElementsByClassName("Btn");
-var span = document.getElementsByClassName("close")[0];
-const body = document.body;
-btn[0].onclick = function() {
-  modal.style.display = "block";
-  body.style.height = '100vh';
-  body.style.overflowY = 'hidden';
-};
-btn[1].onclick = function() {
-  modal.style.display = "block";
-  body.style.height = '100vh';
-  body.style.overflowY = 'hidden';
-};
-span.onclick = function() {
-  modal.style.display = "none";
-};
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
-*/
 
